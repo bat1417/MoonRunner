eef1d1f0575c95e565466f7c7023dd51efcced85
OE9BKJ <97668354+bat1417@users.noreply.github.com>
2023-07-30 20:50:07 +0200

    Update setup.cmd
    Use a venv

--- moonrunner/setup.cmd
+++ moonrunner/setup.cmd
@@ -1,1 +1,8 @@
-pip install -r requirements.txt
+@echo off
+echo "create virtual environment ..."
+py -m venv env
+echo "activate the virtual environment ..."
+.\env\Scripts\activate
+echo "install the requirements ..."
+py -m pip install -r requirements.txt
+echo "finished! You can start MoonRunner by double-click on moonrunner_gui.py or 'py moonrunner_gui.py' in a cmd line"
