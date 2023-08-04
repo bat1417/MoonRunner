7036cee41c8a65f5bf66d03a1bfa4173e2fca9ce
klaus <klaus.battlogg@htldornbirn.at>
2023-08-04 13:45:47 +0200

    Changed Windows Install-Script

--- moonrunner/setup.cmd
+++ moonrunner/setup.cmd
@@ -1,19 +1,19 @@
 @echo off
 REM Check if Python is installed
-where python > nul 2>&1
+python --version 2>nul | findstr /i "Python 3" >nul
 
 IF %ERRORLEVEL% NEQ 0 (
-    echo Python is not installed on this system.
-    echo Please install Python from https://www.python.org/downloads/
+    echo Python 3 is not installed on this system.
+    echo Please install Python 3 from https://www.python.org/downloads/
     exit /b 1
 ) else (
     echo 'found Python:'
     python --version
 )
 echo "create virtual environment ..."
-py -m venv env
+python -m venv env
 echo "activate the virtual environment ..."
 .\env\Scripts\activate
 echo "install the requirements ..."
-py -m pip install -r requirements.txt
-echo "finished! You can start MoonRunner by double-click on moonrunner_gui.py or 'py moonrunner_gui.py' in a cmd line"
+python -m pip install -r requirements.txt
+echo "finished! You can start MoonRunner by double-click on moonrunner_gui.py or 'python moonrunner_gui.py' in a cmd line"
